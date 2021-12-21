acbdfc054edc830f4d460ccebbdf52a516177cf2
PHANAGUS <phanarin03072002@gmail.com>
2021-12-21 13:54:13 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,31 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main()
+{
+    int grade;
+    srand(time(0));
+
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i=0 ; i<3 ; i++)
+    {
+        cin.get();
+    }
+    grade = rand() % 9;
+
+    cout << "You will get ";
+    switch(grade)
+    {
+        case 0: cout << "A"; break;
+        case 1: cout << "B+"; break;
+        case 2: cout << "B"; break;
+        case 3: cout << "C+"; break;
+        case 4: cout << "C"; break;
+        case 5: cout << "D+"; break;
+        case 6: cout << "D"; break;
+        case 7: cout << "F"; break;
+        case 8: cout << "W"; break;
+    }
+    cout << " in this 261102.";
+}
